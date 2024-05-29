bb24a64e58b74626a74782346427fdcc6ec60726
Kushal8616 <Kanjanap@flemingcollege.ca>
2024-05-29 00:01:22 -0400

    modified both script files
    -deleted print from script1.py
    -added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print lines from Zen of python 
 print("there should be one-- and perferably only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're dutch.")
