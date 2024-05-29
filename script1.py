b10c735da10f55352f06227f9bdafc5b4152b4f6
Kushal8616 <Kanjanap@flemingcollege.ca>
2024-05-28 23:56:25 -0400

    added to comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+#print lines from Zen of python 
 print("there should be one-- and perferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're dutch.")
