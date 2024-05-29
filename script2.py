93984d80f9a9571783744ede024e4abd5f76a079
Kushal8616 <Kanjanap@flemingcollege.ca>
2024-05-29 01:09:39 -0400

    modified both files in the repo clone
    -deleted a print() statement from script1.py
    -added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #this is good progarmming advice 
 print("flat is better than nested.")
 print("complex is better than complicated.")
+print("if the implementation is hard to explain, it's a bad idea")
